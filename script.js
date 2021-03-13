97db066f8a0b14434ee1aea8ec658007334c3042
Aram Barnett <aram@arambarnett.com>
2021-03-12 18:28:03 -0800

    fixed bug

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 window.onload = () => {
-     let places = staticLoadPlaces();
-     renderPlaces(places);
+    let places = staticLoadPlaces();
+    renderPlaces(places);
 };
 
 function staticLoadPlaces() {
-    return [
-        {
-            name: 'coconut_palm_tree',
-            location: {
-                lat: 33.973690,
-                lng: -118.371050,
-            }
-        },
-    ];
+   return [
+       {
+           name: 'Magnemite',
+           location: {
+               lat: 33.973690,
+               lng: -118.371050,
+           }
+       },
+   ];
 }
 
 function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
+   let scene = document.querySelector('a-scene');
 
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
+   places.forEach((place) => {
+       let latitude = place.location.lat;
+       let longitude = place.location.lng;
 
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/coconut_palm_tree/scene.gltf');
-        model.setAttribute('rotation', '0 180 0');
-        model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.5 0.5 0.5');
+       let model = document.createElement('a-entity');
+       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+       model.setAttribute('rotation', '0 180 0');
+       model.setAttribute('animation-mixer', '');
+       model.setAttribute('scale', '0.5 0.5 0.5');
 
-        model.addEventListener('loaded', () => {
-            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-        });
+       model.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+       });
 
-        scene.appendChild(model);
-    });
+       scene.appendChild(model);
+   });
 }
