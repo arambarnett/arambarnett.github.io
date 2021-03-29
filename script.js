f098a810004a58b6945f59b7181978371bff9507
Aram Barnett <aram@arambarnett.com>
2021-03-29 15:49:33 -0700

    added new assets

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function staticLoadPlaces() {
    return [
        {
            // nam's address
-           name: 'coconut_palm_tree',
+           name: 'T1',
            location: {
                lat: 40.7438966,
                lng: -73.9928782,
@@ -23,7 +23,7 @@ function staticLoadPlaces() {
 
            },
            // Aram's Address
-           name: 'coconut_palm_tree',
+           name: 'T1',
            location: { 
                lat: 33.973690,
                lng: -118.371050,
@@ -31,7 +31,7 @@ function staticLoadPlaces() {
                  
            },
                // Will's Address
-            name: 'coconut_palm_tree',
+            name: 'T1',
             location: {
                 lat: 34.004490,
                 lng: -118.448463,
@@ -57,7 +57,7 @@ function renderPlaces(places) {
 
        let model = document.createElement('a-entity');
        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-       model.setAttribute('gltf-model', './assets/coconut_palm_tree/scene.gltf');
+       model.setAttribute('gltf-model', './assets/T1/scene.gltf');
        model.setAttribute('rotation', '0 180 0');
        model.setAttribute('animation-mixer', '');
        model.setAttribute('scale', '1 1 1');
